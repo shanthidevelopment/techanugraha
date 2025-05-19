c1a68b6d2fc6a8e5d79f598e3d0f3d04256bb70a
Shanthi it solution <development@shanthiitsolution.com>
2025-05-19 16:52:19 +0530

    Update views.py

--- website/views.py
+++ website/views.py
@@ -6,3 +6,58 @@ def home(request):
 
 def bfsi(request):
     return render(request,"bfsi.html")
+
+
+def cpp_view(request):
+    return render(request, 'cpp.html')
+
+
+def java(request):
+    return render(request, 'java.html')
+
+
+def python(request):
+    return render(request, 'python.html')
+
+
+def web(request):
+    return render(request, 'web.html')
+
+
+def mern(request):
+    return render(request, 'mern.html')
+
+
+def mob(request):
+    return render(request, 'mob.html')
+
+def react(request):
+    return render(request, 'react.html')
+
+
+def sql(request):
+    return render(request, 'sql.html')
+
+
+def ecom(request):
+    return render(request, 'ecom.html')
+
+
+def dm(request):
+    return render(request, 'dm.html')
+
+
+def report(request):
+    return render(request, 'report.html')
+
+
+def custom(request):
+    return render(request, 'custom.html')
+
+
+def business(request):
+    return render(request, 'business.html')
+
+
+def tax(request):
+    return render(request, 'tax.html')
