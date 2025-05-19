e0e6412b41ff2940a1a479db3d8e59ecfd169da4
Shanthi it solution <development@shanthiitsolution.com>
2025-05-19 16:52:35 +0530

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path
 from .import views
 
+
 urlpatterns = [
     path('',views.home,name='/'),
-    path('bfsi/',views.bfsi,name='bfsi'),
+    path('courses/bfsi/',views.bfsi,name='bfsi'),
+    path('courses/cpp/', views.cpp_view, name='cpp'),
+    path('courses/java/', views.java, name='java'),
+    path('courses/python/', views.python, name='python'),
+    path('courses/web/', views.web, name='web'),
+    path('courses/mern/', views.mern, name='mern'),
+    path('courses/mobile/', views.mob, name='mob'),
+    path('courses/react/', views.react, name='react'),
+    path('courses/sql/', views.sql, name='sql'),
+    path('courses/ecom/', views.ecom, name='ecom'),
+    path('courses/dm/', views.dm, name='dm'),
+    path('courses/report/', views.report, name='report'),
+     path('courses/crm/', views.custom, name='custom'),
+     path('courses/business/', views.business, name='business'),
+path('courses/tax/', views.tax, name='tax'),
 ]
