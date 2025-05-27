8c5f74a6d7a2f3f9bbaeea428f8c86fe64c2d12a
Shanthi it solution <development@shanthiitsolution.com>
2025-05-27 15:19:53 +0530

    Update settings.py

--- techanugraha/settings.py
+++ techanugraha/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     'website',
+     'corsheaders',
+
 ]
 
 MIDDLEWARE = [
@@ -51,9 +53,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'techanugraha.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
